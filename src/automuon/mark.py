01b23263860198262164482f89e22ef55260bb3a
310hz <90894519+310hz@users.noreply.github.com>
2026-07-29 00:08:45 +0900

    typehint

--- src/automuon/mark.py
+++ src/automuon/mark.py
@@ -1,24 +1,29 @@
+from typing import TypeVar
+
 import torch.nn as nn
 
 
-def with_muon(param):
+_Markable = nn.Module | nn.Parameter
+_MarkableT = TypeVar("_MarkableT", bound=_Markable)
+
+def with_muon(param: _MarkableT) -> _MarkableT:
     _mark(param, True)
     return param
 
-def without_muon(param):
+def without_muon(param: _MarkableT) -> _MarkableT:
     _mark(param, False)
     return param
 
-def with_adam(param):
+def with_adam(param: _MarkableT) -> _MarkableT:
     _mark(param, False)
     return param
 
-def without_adam(param):
+def without_adam(param: _MarkableT) -> _MarkableT:
     _mark(param, True)
     return param
 
 
-def _mark(param, flag):
+def _mark(param: _Markable, flag: bool) -> None:
     if isinstance(param, nn.Module):
         for p in param.parameters():
             setattr(p, "_automuon_flag", flag)
