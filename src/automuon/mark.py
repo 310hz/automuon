425a04e0f8c9b9c24dadb3cd16573b20c2c78d00
310hz <90894519+310hz@users.noreply.github.com>
2026-07-28 23:30:44 +0900

    fix bug and refactor

--- src/automuon/mark.py
+++ src/automuon/mark.py
@@ -1,17 +1,6 @@
 import torch.nn as nn
 
 
-def _mark(params, flag):
-    if isinstance(params, (nn.Parameter, nn.Module)):
-        params = [params]
-    for param in params:
-        if isinstance(param, nn.Module):
-            for p in param.parameters():
-                setattr(p, "_automuon_flag", flag)
-        else:
-            setattr(param, "_automuon_flag", flag)
-
-
 def with_muon(params):
     _mark(params, True)
     return params
@@ -21,9 +10,26 @@ def without_muon(params):
     return params
 
 def with_adam(params):
-    _mark(params, True)
+    _mark(params, False)
     return params
 
 def without_adam(params):
-    _mark(params, False)
+    _mark(params, True)
     return params
+
+
+def _mark(params, flag):
+    if isinstance(params, (nn.Parameter, nn.Module)):
+        params = [params]
+    for param in params:
+        if isinstance(param, nn.Module):
+            for p in param.parameters():
+                setattr(p, "_automuon_flag", flag)
+        elif isinstance(param, nn.Parameter):
+            setattr(param, "_automuon_flag", flag)
+        else:
+            raise TypeError(
+                "Expected the input to a marking function to be an "
+                "instance of torch.nn.Parameter or torch.nn.Module, "
+                f"but got '{type(param)}'."
+            )
