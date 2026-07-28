1ef70316521a4ba9ab5b545c2f296fb1e05b9485
310hz <90894519+310hz@users.noreply.github.com>
2026-07-28 18:08:18 +0900

    update mark.py

--- src/automuon/mark.py
+++ src/automuon/mark.py
@@ -1,16 +1,27 @@
 import torch.nn as nn
 
 
-def use_muon(
-    params: nn.Parameter | nn.Module | list[nn.Parameter | nn.Module]
-) -> nn.Parameter | nn.Module | list[nn.Parameter | nn.Module]:
+def _mark(params, flag):
     original = params
     if isinstance(params, (nn.Parameter, nn.Module)):
         params = [params]
     for param in params:
         if isinstance(param, nn.Module):
             for p in param.parameters():
-                setattr(p, "_automuon_use_muon", True)
+                setattr(p, "_automuon_flag", flag)
         else:
-            setattr(param, "_automuon_use_muon", True)
+            setattr(param, "_automuon_flag", flag)
     return original
+
+
+def with_muon(params):
+    return _mark(params, True)
+
+def without_muon(params):
+    return _mark(params, False)
+
+def with_adam(params):
+    return _mark(params, True)
+
+def without_adam(params):
+    return _mark(params, False)
