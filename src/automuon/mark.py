f7e6364a9f030a681332c437663540775539b61d
310hz <90894519+310hz@users.noreply.github.com>
2026-07-28 23:56:46 +0900

    list input 無効

--- src/automuon/mark.py
+++ src/automuon/mark.py
@@ -1,35 +1,32 @@
 import torch.nn as nn
 
 
-def with_muon(params):
-    _mark(params, True)
-    return params
+def with_muon(param):
+    _mark(param, True)
+    return param
 
-def without_muon(params):
-    _mark(params, False)
-    return params
+def without_muon(param):
+    _mark(param, False)
+    return param
 
-def with_adam(params):
-    _mark(params, False)
-    return params
+def with_adam(param):
+    _mark(param, False)
+    return param
 
-def without_adam(params):
-    _mark(params, True)
-    return params
+def without_adam(param):
+    _mark(param, True)
+    return param
 
 
-def _mark(params, flag):
-    if isinstance(params, (nn.Parameter, nn.Module)):
-        params = [params]
-    for param in params:
-        if isinstance(param, nn.Module):
-            for p in param.parameters():
-                setattr(p, "_automuon_flag", flag)
-        elif isinstance(param, nn.Parameter):
-            setattr(param, "_automuon_flag", flag)
-        else:
-            raise TypeError(
-                "Expected the input to a marking function to be an "
-                "instance of torch.nn.Parameter or torch.nn.Module, "
-                f"but got '{type(param)}'."
-            )
+def _mark(param, flag):
+    if isinstance(param, nn.Module):
+        for p in param.parameters():
+            setattr(p, "_automuon_flag", flag)
+    elif isinstance(param, nn.Parameter):
+        setattr(param, "_automuon_flag", flag)
+    else:
+        raise TypeError(
+            "Expected the input to a marking function to be an "
+            "instance of torch.nn.Parameter or torch.nn.Module, but "
+            f"got '{type(param)}'."
+        )
