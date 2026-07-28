a38af8cf71a5c09aa05baf0fa21f0f4c663b5c76
310hz <90894519+310hz@users.noreply.github.com>
2026-07-28 23:31:20 +0900

    init.py

--- src/automuon/__init__.py
+++ src/automuon/__init__.py
@@ -1,3 +1,6 @@
 from importlib.metadata import version
 
+from .optimizer import get_muon_with_adam
+from .mark import with_muon, without_muon, with_adam, without_adam
+
 __version__ = version("automuon")
